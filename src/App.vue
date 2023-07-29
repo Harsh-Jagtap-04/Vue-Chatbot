82c519c6dda495e8856131f92ceb486582370b14
Harsh-Jagtap-04 <harshjagtap0401@gmail.com>
2023-07-29 10:30:38 +0530

    pdf added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="container mx-auto">
     <ChatBox />
+    <vue-pdf-embed :source="pdfUrl" :width="900" :height="600" />
   </div>
 </template>
 
 <script>
 import ChatBox from './components/ChatBox.vue';
+import VuePdfEmbed from 'vue-pdf-embed';
 
 export default {
   name: 'App',
   components: {
     ChatBox,
+    VuePdfEmbed,
+  },
+  data() {
+    return {
+      pdfUrl: 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf',
+    };
   },
 };
 </script>
